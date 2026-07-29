86efa8a42dc19522e2d1a734a13571bf59da3fb9
Jason Wong <wongkj.11@gmail.com>
2026-07-29 10:14:02 +1000

    feat: Adding the about and information pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const AboutPage: React.FC = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
