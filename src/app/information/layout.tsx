5eda02ccb2fc426e2697599bbd975330746acf6f
Jason Wong <wongkj.11@gmail.com>
2026-07-29 12:10:02 +1000

    feat: Adding a top and bottom gutter as well

--- src/app/information/layout.tsx
+++ src/app/information/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
-      <div className="mx-auto px-40">{children}</div>
+      <div className="mx-auto px-40 my-auto py-20">{children}</div>
     </>
   );
 }
